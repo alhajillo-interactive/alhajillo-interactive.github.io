54c2208af6094410e494249b8b7ebb1932a66a00
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2025-10-15 23:02:57 -0500

    Install vituum and nunjucks templates

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import vituum from "vituum";
+import nunjucks from "@vituum/vite-plugin-nunjucks";
+
+export default defineConfig({
+    plugins: [vituum(), nunjucks({ root: "./src" })],
+});
